f611af09e66c1bdc4b4bc9879ec14d9213ff3431
unknown <mahseeen@gmail.com>
2019-11-04 19:20:28 +0300

    refactor: change costructor

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
 const Keyboard = class {
     constructor(){
-        this.shiftUnpressedEng = [
-            ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
-            ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
-            ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
-            ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '↑', '/'],
-            ['Ctrl', 'Alt', 'Space', '←', '↓', '→']
-          ],
-          this.shiftPressedEng = [
-            ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'],
-            ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|'],
-            ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter'],
-            ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '↑', '?'],
-            ['Ctrl', 'Alt', 'Space', '←', '↓', '→']
-          ],
-          this.shiftUnpressedRus = [
-              ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
-              ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\'],
-              ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
-              ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '↑', '.'],
-              ['Ctrl', 'Alt', 'Space', '←', '↓', '→']
-          ],
-          this.shiftPressedRus = [
-              ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace'],
-              ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\'],
-              ['CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter'],
-              ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '↑', ','],
-              ['Ctrl', 'Alt', 'Space', '←', '↓', '→']
-          ],
-          this.body = document.querySelector('body'),
-          this.btnsArray = [
-            [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8],
-            [9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220],
-            [20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13],
-            [16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 38, 191],
-            [17, 18, 32, 37, 40, 39]],
-          this.allBtns = [[],[],[],[],[]]
+        this.eng = {
+            shiftUnpressed : [
+                ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+                ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
+                ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
+                ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '↑', '/'],
+                ['Ctrl', 'Alt', 'Space', '←', '↓', '→']
+                ],
+            shiftPressed : [
+                ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'],
+                ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|'],
+                ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter'],
+                ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '↑', '?'],
+                ['Ctrl', 'Alt', 'Space', '←', '↓', '→']
+                ]
+        }
+        this.rus = {
+            shiftUnpressed : [
+                ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+                ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\'],
+                ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
+                ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '↑', '.'],
+                ['Ctrl', 'Alt', 'Space', '←', '↓', '→']
+            ],
+            shiftPressed : [
+                ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace'],
+                ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\'],
+                ['CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter'],
+                ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '↑', ','],
+                ['Ctrl', 'Alt', 'Space', '←', '↓', '→']
+            ],
+        }
+        this.body = document.querySelector('body'),
+        this.btnsArray = [
+          [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8],
+          [9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220],
+          [20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13],
+          [16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 38, 191],
+          [17, 18, 32, 37, 40, 39]],
+        this.allBtns = [[],[],[],[],[]]
     }
     create() {
         this.body = document.querySelector('body')
@@ -146,7 +150,7 @@ const Keyboard = class {
         }
         for(let k = 0; k < this.allBtns.length; k++)
         for(let l = 0; l < this.allBtns[k].length; l++){
-            this.allBtns[k][l].innerText = this.shiftUnpressedRus[k][l]
+            this.allBtns[k][l].innerText = this.rus.shiftUnpressed[k][l]
         }
     
         document.addEventListener('keydown', () => {
@@ -171,7 +175,7 @@ const Keyboard = class {
                         this.input.value += '\n'
                     break;       
                 case 16:
-                        this.shifted(this.shiftPressedRus)
+                        this.shifted(this.rus.shiftPressed)
                     break;
                 case 17:
                         console.log('ctrl');
@@ -207,11 +211,11 @@ const Keyboard = class {
                 } 
             }
             if (!event.shiftKey) {
-                this.unshifted(this.shiftUnpressedRus)
+                this.unshifted(this.rus.shiftUnpressed)
             }
             if (event.getModifierState("CapsLock")) {
                 document.querySelector('.caps').classList.add('pressed');
-                this.shifted(this.shiftPressedRus)
+                this.shifted(this.rus.shiftPressed)
             } else {
                 document.querySelector('.caps').classList.add('keyup');
             }
@@ -237,7 +241,7 @@ const Keyboard = class {
                             this.input.value += '\n'
                         break;       
                     case 'Shift':
-                            this.shifted(this.shiftPressedRus)
+                            this.shifted(this.rus.shiftPressed)
                         break;
                     case 'Ctrl':
                             console.log('ctrl');
@@ -259,7 +263,7 @@ const Keyboard = class {
                 switch (btn.innerHTML) {
                     case 'Shift':
                         console.log('caps');
-                        this.shifted(this.shiftUnpressedRus)
+                        this.shifted(this.rus.shiftUnpressed)
                         break;
                 }
             })
